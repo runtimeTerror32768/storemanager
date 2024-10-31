5095e0d01479804ce4fc8aab873ab9ff9f5c1c90
dorin c <dorinionut.cherciu@gmail.com>
2024-10-31 19:32:25 +0200

    Initial commit

---
+++ src/main/java/org/cdi/storemanager/StoreManagerApplication.java
@@ -0,0 +1,13 @@
+package org.cdi.storemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagerApplication.class, args);
+	}
+
+}
